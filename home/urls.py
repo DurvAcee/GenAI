1a7b51beb62c1e50522aef253c39b9b8e85d7887
Durvesh Danve <Durvesh.Danve@veritas.com>
2024-05-26 18:06:54 +0530

    Updated API

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from home import views
+from .views import fetch_initial_message
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
-    path('service', views.service, name='service')
+    path('service', views.service, name='service'),
+    path('localbot/welcome/', fetch_initial_message, name='fetch_initial_message'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
+from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
+import requests
 
 # Create your views here.
 def index(request):
@@ -6,6 +8,17 @@ def index(request):
         'variable' : "This is sent!"
     }
     return render(request, 'index.html', context)
+
+# /localbot/welcome/
+def fetch_initial_message(request):
+    api_url = 'https://mocki.io/v1/ad9c060a-dd91-427f-b47c-99706b866d88'                
+    try:
+        response = requests.get(api_url, verify=False)
+        response.raise_for_status()
+        data = response.json()
+        return JsonResponse(data)
+    except requests.RequestException as e:
+        return JsonResponse({'error': str(e)}, status=500)
 
 def about(request):
    return render(request, 'about.html')
